de8efb0523ece60cce7357e12cc75af258f943e6
Henderson <hendersonc@trinityprep.org>
2026-09-10 20:27:29 -0400

    sedfrgb

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/RobotConstants.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.common;
+
+public class RobotConstants {
+
+    public static class Drivetrain
+    {
+        public static final String LEFT_FRONT = "leftFrontDrive";
+        public static final String RIGHT_FRONT = "rightFrontDrive";
+        public static final String LEFT_REAR = "leftRearDrive";
+        public static final String RIGHT_REAR = "rightRearDrive";
+    }
+}
